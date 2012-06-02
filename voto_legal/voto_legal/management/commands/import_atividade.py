745a88299cc2acfffc7a017d26c4d1ea4ba8cac8
Rodrigo Machado <rcmachado@gmail.com>
2012-06-01 23:50:27 -0300

    Import de atividades partidárias

--- voto_legal/voto_legal/management/commands/import_atividade.py
+++ voto_legal/voto_legal/management/commands/import_atividade.py
@@ -1,24 +1,29 @@
 # coding: utf-8
-import urllib
-#from bs4 import BeautifulSoup
+import re
+
 from pyquery import PyQuery
 from django.core.management.base import BaseCommand
 
-from voto_legal.models import CasaGovernamental
+from voto_legal.models import (CasaGovernamental, CategoriaProjeto, Politico,
+    PoliticoCategoriaProjeto)
 
 
 class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         for casa in CasaGovernamental.objects.all():
-            self._parse_politicos_projetos(casa.id)
+            print u"Importando dados de {0}".format(casa.nome)
+            self._parse_politicos_projetos(casa.id_referencia)
 
-    def _parse_politicos_projetos(casa_id):
+    def _parse_politicos_projetos(self, casa_id):
         url = "http://www.excelencias.org.br/@pls.php?cs={0}".format(casa_id)
         document = PyQuery(url=url)
         doc = document.find('#restringe').eq(1).find('tr')
+        # javascript:lpls(8362,'QWJlbGFyZG8gQ2FtYXJpbmhh',1)
+        link_regex = re.compile('javascript\:lpls\((\d+),')
 
         politicos = {}
         last_name = None
+        last_id_referencia = None
         abort_user = False
         for i in xrange(0, len(doc)):
             elem = doc.eq(i)
@@ -26,12 +31,22 @@ class Command(BaseCommand):
             if nome:
                 politicos[nome] = {}
                 last_name = nome
+                link = elem.find('td b a')[0].attrib.get('href')
+                found = link_regex.match(link)
+                last_id_referencia = int(found.group(1))
                 abort_user = False
                 continue
             # Ainda não chegou nas células com os dados
             if last_name is None:
                 continue
             if abort_user:
+                continue
+
+            print u"\tImportando dados do politico {0} ({1})".format(last_name, last_id_referencia)
+            try:
+                politico = Politico.objects.get(id_transparencia=last_id_referencia)
+            except Politico.DoesNotExist:
+                print u"\tPolítico {0} ({1}) não existe".format(last_name, last_id_referencia)
                 continue
 
             titulo_categoria = elem.find('td i')
@@ -45,14 +60,25 @@ class Command(BaseCommand):
                         abort_user = True
                         break
                     categ_titulo = categ_base.eq(0).html()
-                    categ_valor_a = categ_base.eq(1).html()
-                    categ_valor_b = categ_base.eq(2).html()
+                    categ_valor_a = categ_base.eq(1).html().strip() or 0
+                    categ_valor_b = categ_base.eq(2).html().strip() or 0
                     politicos[last_name][categ_titulo] = {
-                        'sem_relevancia': categ_valor_a.strip(),
-                        'outros': categ_valor_b.strip(),
+                        'sem_relevancia': categ_valor_a,
+                        'outros': categ_valor_b,
                     }
-                    #print "%s: %s / %s" % (last_name, repr(categ_titulo), repr(categ_valor_b))
+                    # É uma categoria relevante
+                    if categ_valor_b:
+                        relevante = True
+                        valor = categ_valor_b
+                    else:
+                        relevante = False
+                        valor = categ_valor_a
+                    categoria_projeto, _ = CategoriaProjeto.objects.get_or_create(nome=categ_titulo, relevante=relevante)
+
+                    pcp, _ = PoliticoCategoriaProjeto.objects.get_or_create(politico=politico, categoria_projeto=categoria_projeto)
+                    pcp.quantidade = valor
+                    pcp.save()
+                    print u"\t\t{0}: {1} / {2}".format(categ_titulo, categ_valor_a, categ_valor_b)
                 if abort_user:
                     continue
         return politicos
-
