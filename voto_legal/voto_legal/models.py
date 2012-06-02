ba9c74b007c3996a3905200b29334abc97f25253
Rodrigo Machado <rcmachado@gmail.com>
2012-06-02 10:30:49 -0300

    Ajuste no perfil

--- voto_legal/voto_legal/models.py
+++ voto_legal/voto_legal/models.py
@@ -146,6 +146,7 @@ class FacebookProfileManager(object):
         for friend in my_friends.all():
             data = friend.get_facebook_profile()
             response.append({
+                'fb_id': data['id'],
                 'name': data['name'],
                 'username': data['username'],
                 'picture': data['picture'],
